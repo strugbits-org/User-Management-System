e914feb1774a5a03c4e702d3b43b9cfbb746e9b0
waleed <waleedshahid006@gmail.com>
2021-09-01 18:12:59 +0500

    changes

--- src/controller/MessageController.js
+++ src/controller/MessageController.js
@@ -32,9 +32,15 @@ const getMessages = async (req, res) => {
       user1Id: user1[0].userId,
       user1Image: user1[0].userImage,
       user1Name: user1[0].firstName + " " + user1[0].lastName,
+      user1Email: user1[0].email,
+      user1Location:user1[0].city + ", " + user1[0].country,
+      user1Employment:user1[0].employment,
       user2Id: user2[0].userId,
       user2Image: user2[0].userImage,
       user2Name: user2[0].firstName + " " + user2[0].lastName,
+      user2Email: user2[0].email,
+      user2Location:user2[0].city + ", " + user2[0].country,
+      user2Employment:user2[0].employment,
     };
     res.status(200).json({ messages, userImages });
   } catch (err) {
